d42d3b1e39883b79c363981e76e9458c06193da0
JYOTSNA1330 <jyotsna1330.jr@gmail.com>
2017-03-22 12:12:36 +0530

    Add files via upload

---
+++ javascript/m2/script.js
@@ -0,0 +1,55 @@
+// Event handling
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("data/name.json", 
+            function (res) {
+              var temp =
+   document.getElementById("name").value;
+                 
+              if ( temp === res.student1) {
+                var temp2= res.question1
+                if (true) {
+                  var result = "Correct answer!"
+
+                }
+                else {
+                  var result = "Wrong answer!"
+                }
+                
+              }
+              else if (temp === res.student2) {
+                var temp2= res.question2
+                if (true) {
+                  var result = "Correct answer!"
+
+                }
+                else {
+                  var result = "Wrong answer!"
+                }
+                
+              }
+
+          else {
+            var temp1 = "no such student";
+          }
+              document.querySelector("#content")
+                .innerHTML = "<h2>" + temp+ "</h2>";
+
+              document.querySelector("#content")
+                .innerHTML = "<h2>" + temp2+ "</h2>";
+              document.querySelector("#content1")
+                .innerHTML = "<h2>" + temp1+ "</h2>";
+              document.querySelector("#content2")
+                .innerHTML = "<h2>" + result+ "</h2>";
+            });
+      });
+  
+  }
+);
