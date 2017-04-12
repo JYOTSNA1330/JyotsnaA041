cc1be6b6647d7d739545c017fc0478018511fc3b
JYOTSNA1330 <jyotsna1330.jr@gmail.com>
2017-04-12 11:38:22 +0530

    Add files via upload

---
+++ wp/script.js
@@ -0,0 +1,37 @@
+// Event handling
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    // Unobtrusive event binding
+    document.querySelector("#btn")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+	$ajaxUtils
+          .sendGetRequest("name.json", 
+            function (res) {
+	      var num = Number(document.getElementById("fname").value); 
+	      var arr = res.arr;
+	      var arr2 = res.arr2; 
+	      var arr3 = res.arr3;
+	      var message = "not found"; 
+
+	    for(var i = 0; i < 10; i++){
+	      	console.log(arr[i]); 
+		if(arr[i] == num){
+		   	   message = arr2[i]; 
+		   	   message1 = arr3[i];
+	           break; 
+		}
+	      }
+		
+              document.querySelector("#content").innerHTML = "<h2>" + message + "</h2>";
+              document.querySelector("#content").innerHTML = "<h2>" + message1 + "</h2>";
+            });
+
+
+ 		
+      });
+  }
+);
+
